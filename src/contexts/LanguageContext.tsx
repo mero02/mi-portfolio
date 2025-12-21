625a00b74d7730f8cd8a6c02d87053cb2a46397b
mero02 <mero2sp@gmail.com>
2025-12-21 10:06:24 -0300

    feat: Agregar componentes para traduccion

---
+++ src/contexts/LanguageContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+type Language = 'en' | 'es';
+
+interface LanguageContextType {
+  language: Language;
+  changeLanguage: (lang: Language) => void;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState<Language>(() => {
+    const saved = localStorage.getItem('language');
+    return (saved as Language) || 'es';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('language', language);
+    i18n.changeLanguage(language);
+  }, [language, i18n]);
+
+  const changeLanguage = (lang: Language) => {
+    setLanguage(lang);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) throw new Error('useLanguage must be used within LanguageProvider');
+  return context;
+};
